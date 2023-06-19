63a121506d6824bb77bf2431ea45c92d7148ade5
Andreas Sundqvist <andreas@stefna.is>
2023-06-19 09:24:30 +0200

    [di] Extract argument resolving to trait
    This is to avoid copying this code when we need this for custom 
    argument resolving

---
+++ src/Helper/ArgumentResolverTrait.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+
+namespace Stefna\DependencyInjection\Helper;
+
+use Psr\Container\ContainerInterface;
+use ReflectionAttribute;
+use Stefna\DependencyInjection\Exception\NotResolvedException;
+use Stefna\DependencyInjection\Helper\Attribute\ConfigureAttribute;
+use Stefna\DependencyInjection\Helper\Attribute\ResolverAttribute;
+
+trait ArgumentResolverTrait
+{
+	/**
+	 * @return list<mixed>
+	 */
+	private function resolveArguments(\ReflectionMethod $method, ContainerInterface $container): array
+	{
+		$args = [];
+		$params = $method->getParameters();
+		foreach ($params as $param) {
+			$args[] = $this->resolveArgument($param, $container);
+		}
+		return $args;
+	}
+
+	private function resolveArgument(\ReflectionParameter $param, ContainerInterface $container): mixed
+	{
+		$type = $param->getType();
+		if (!$type instanceof \ReflectionNamedType) {
+			throw new NotResolvedException('Can\'t auto-wire complex types');
+		}
+
+		$typeName = $type->getName();
+		$resolvableAttrs = $param->getAttributes(ResolverAttribute::class, ReflectionAttribute::IS_INSTANCEOF);
+		$paramInstance = null;
+		if ($resolvableAttrs) {
+			foreach ($resolvableAttrs as $reflectionAttribute) {
+				/** @var ResolverAttribute $attr */
+				$attr = $reflectionAttribute->newInstance();
+				$paramInstance = $attr->resolve($typeName, $container);
+			}
+		}
+
+		if (!$paramInstance && ($type->isBuiltin() || !$container->has($typeName))) {
+			return $this->resolveDefault($param);
+		}
+		$paramInstance = $paramInstance ?? $container->get($typeName);
+
+		if (is_object($paramInstance)) {
+			$configureAttributes = $param->getAttributes(ConfigureAttribute::class, ReflectionAttribute::IS_INSTANCEOF);
+			foreach ($configureAttributes as $reflectionAttribute) {
+				$attr = $reflectionAttribute->newInstance();
+				$attr->configure($paramInstance, $container);
+			}
+		}
+
+		return $paramInstance;
+	}
+
+	private function resolveDefault(\ReflectionParameter $param): mixed
+	{
+		if ($param->isDefaultValueAvailable()) {
+			return $param->getDefaultValue();
+		}
+		throw new NotResolvedException(sprintf(
+			'Can\'t resolve argument "$%s" of type "%s"',
+			$param->getName(),
+			(string)$param->getType()?->__toString(),
+		));
+	}
+}
--- src/Helper/Autowire.php
+++ src/Helper/Autowire.php
@@ -10,6 +10,8 @@ use Stefna\DependencyInjection\Helper\Attribute\ResolverAttribute;
 
 final class Autowire
 {
+	use ArgumentResolverTrait;
+
 	/**
 	 * @param class-string|null $className
 	 */
@@ -30,64 +32,16 @@ final class Autowire
 	 */
 	public function __invoke(ContainerInterface $container, string $className): object
 	{
-		$reflection = new \ReflectionClass($this->className ?? $className);
+		$className = $this->className ?? $className;
+		$reflection = new \ReflectionClass($className);
 		$constructor = $reflection->getConstructor();
 		$params = $constructor?->getParameters() ?? [];
 		$args = [];
 		foreach ($params as $param) {
-			$type = $param->getType();
-			if (!$type instanceof \ReflectionNamedType) {
-				throw new NotResolvedException('Can\'t auto-wire complex types');
-			}
-
-			/** @var class-string $typeName */
-			$typeName = $type->getName();
-			$resolvableAttrs = $param->getAttributes(ResolverAttribute::class, ReflectionAttribute::IS_INSTANCEOF);
-			$paramInstance = null;
-			if ($resolvableAttrs) {
-				foreach ($resolvableAttrs as $reflectionAttribute) {
-					/** @var ResolverAttribute $attr */
-					$attr = $reflectionAttribute->newInstance();
-					$paramInstance = $attr->resolve($typeName, $container);
-				}
-			}
-
-			if (!$paramInstance && $type->isBuiltin()) {
-				if ($param->isOptional()) {
-					continue;
-				}
-				throw new NotResolvedException(sprintf(
-					'Can\'t resolve argument "$%s" of type "%s"',
-					$param->getName(),
-					(string)$param->getType()?->__toString(),
-				));
-			}
-
-			if (!$paramInstance && !$container->has($typeName)) {
-				if ($param->isOptional()) {
-					continue;
-				}
-				throw new NotResolvedException(sprintf(
-					'Can\'t resolve argument "$%s" of type "%s"',
-					$param->getName(),
-					(string)$param->getType()?->__toString(),
-				));
-			}
-			/** @var object $paramInstance */
-			$paramInstance = $paramInstance ?? $container->get($typeName);
-
-			if (is_object($paramInstance)) {
-				$configureAttributes = $param->getAttributes(ConfigureAttribute::class, ReflectionAttribute::IS_INSTANCEOF);
-				foreach ($configureAttributes as $reflectionAttribute) {
-					$attr = $reflectionAttribute->newInstance();
-					$attr->configure($paramInstance, $container);
-				}
-			}
-
-			$args[] = $paramInstance;
+			$args[] = $this->resolveArgument($param, $container);
 		}
 
-		// @phpstan-ignore-next-line - don't feel like figure out how to make phpstan happy
-		return $reflection->newInstance(...$args);
+		/** @var T of object */
+		return new $className(...$args);
 	}
 }
